8915a9de9945e84fd6310ab33644419c71d48804
Khalid-Alharbi <kjharbi@hotmail.com>
2022-04-04 20:17:33 +0300

    change the dir to the parent-dir

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -136,7 +136,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
       //   onPressed: () {
       //     _scaffoldKey.currentState!.openDrawer();
       //   },
-      // ),
+      // )
     );
   }
 }
